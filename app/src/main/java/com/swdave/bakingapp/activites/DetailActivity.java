cc306d9730e551520ccca964f8edbe7b237fbba9
swdevdave <swdev.dave@gmail.com>
2018-10-30 13:31:07 -0700

    verified incoming intent

--- app/src/main/java/com/swdave/bakingapp/activites/DetailActivity.java
+++ app/src/main/java/com/swdave/bakingapp/activites/DetailActivity.java
@@ -3,6 +3,8 @@ package com.swdave.bakingapp.activites;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.swdave.bakingapp.R;
 import com.swdave.bakingapp.model.Recipe;
@@ -11,17 +13,25 @@ import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private static final String TAG = "DetailActivity";
 
-        ArrayList<Recipe> mRecipeArrayList;
+    private ArrayList<Recipe> mRecipeArrayList;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_detail);
+            Log.d(TAG, "onCreate: Started ");
 
             // Get recipe from intent extra
             Intent recipeIntent = getIntent();
             mRecipeArrayList = recipeIntent.getParcelableArrayListExtra("recipes");
 
+            for (int i = 0; i < mRecipeArrayList.size() ; i++) {
+                Log.d(TAG, "onCreate: " + i + mRecipeArrayList.get(i).getName());
+            }
+
+            Log.d(TAG, "onCreate: Finished ");
 
 
     }
